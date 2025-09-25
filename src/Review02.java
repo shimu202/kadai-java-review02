71bfcd8ca4d1f13fd827ffd9354fe76e5b265a9d
shimu202 <shimuraryou@localhost>
2025-09-26 01:25:02 +0900

    条件の変更

--- src/Review02.java
+++ src/Review02.java
@@ -3,7 +3,7 @@ public class Review02 {
 
     public static void main(String[] args) {
         for(int i = 1; i < 101; i++) {
-            if(i % 15 ==0) {
+            if(i % 5 ==0 && i % 3 == 0) {
                 System.out.println("FizzBuzz");
             }else if(i % 3 ==0) {
                 System.out.println("Fizz");
